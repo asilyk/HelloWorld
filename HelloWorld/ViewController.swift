9433efab44ec5050856597133c6aebe02837aeb9
Alexander Ilyk <asi112@icloud.com>
2021-12-24 09:16:51 +0300

    Swift format fixes

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,10 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet var helloWorldLabel: UILabel!
     @IBOutlet var showTextButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         helloWorldLabel.isHidden = true
@@ -21,13 +20,11 @@ class ViewController: UIViewController {
 
     @IBAction func showTextButtonPressed() {
         helloWorldLabel.isHidden.toggle()
-        
+
         if helloWorldLabel.isHidden {
             showTextButton.setTitle("Show text", for: .normal)
         } else {
             showTextButton.setTitle("Hide text", for: .normal)
         }
     }
-    
 }
-
